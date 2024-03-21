340358a83ecb070b8fa7395fe45214e5d1833091
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-03-21 01:17:27 +0100

    planning to remove the helm chart if this deploys fine

--- helm_charts/tools.tf
+++ helm_charts/tools.tf
@@ -10,3 +10,11 @@ resource "helm_release" "cert_manager" {
     value          = "true"
   }
 }
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+}
